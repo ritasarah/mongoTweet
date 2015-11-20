b304a56e38bc38b719e82dfdf79e3bdce91754d5
unknown <rita_miffies@yahoo.co.id>
2015-11-20 17:34:07 +0700

    first commit

---
+++ src/mongotweet/MongoTweet.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mongotweet;
+
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
+import java.util.List;
+
+/**
+ *
+ * @author ASUS
+ */
+public class MongoTweet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Since 2.10.0, uses MongoClient
+	MongoClient mongo = new MongoClient( "167.205.35.19");
+        
+	MongoDatabase db = mongo.getDatabase("anda");
+
+//        boolean auth = db.authenticate("hadoop", "hadoop".toCharArray());
+        
+        MongoIterable<String> dbs = mongo.listDatabaseNames();
+	for(String adb : dbs){
+		System.out.println(adb);
+	}
+        
+    }
+    
+}
